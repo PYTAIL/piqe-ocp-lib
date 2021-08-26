c2c610549b179ae39aa7095440ddedbdcfe992be
Anandprakash Tandale <atandale@redhat.com>
2021-08-26 21:21:24 +0530

    end
    changes for clean default cluster

--- piqe_ocp_lib/api/tasks/operator_ops.py
+++ piqe_ocp_lib/api/tasks/operator_ops.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List, Optional, Tuple, Union, Dict
+from typing import Dict, List, Optional, Tuple, Union
 
 from kubernetes.client.rest import ApiException
 
@@ -116,19 +116,22 @@ class OperatorInstaller(OcpBase):
         """
         all_sub_resp_obj = self.sub_obj.get_all_subscriptions()
         for i in range(0,len(all_sub_resp_obj.items)):
-            if operator_name in str(all_sub_resp_obj.items[i]):
-                target_item = i
-        csv_name = self.ohp_obj.get_package_channel_by_name(operator_name,
+            if operator_name not in str(all_sub_resp_obj.items[i]):
+                return None
+                break
+            else:
+                target_item = i 
+                csv_name = self.ohp_obj.get_package_channel_by_name(operator_name,
                    all_sub_resp_obj.items[target_item]['spec']['channel']).currentCSV
-        operator_namespace = all_sub_resp_obj.items[target_item]['metadata']['namespace']
-        assert 'channel' in all_sub_resp_obj.items[target_item]['spec'].keys()
-        assert 'sourceNamespace' in all_sub_resp_obj.items[target_item]['spec'].keys()
-        assert 'startingCSV' in all_sub_resp_obj.items[target_item]['spec'].keys()
-        assert 'name' in all_sub_resp_obj.items[target_item]['spec'].keys()
-        assert all_sub_resp_obj.items[target_item]['spec']['name'] == operator_name
-        assert all_sub_resp_obj.items[target_item]['status']['state'] == 'AtLatestKnown'
-        assert self.csv.is_cluster_service_version_present(csv_name, operator_namespace)
-        return all_sub_resp_obj.items[target_item]['spec']    
+                operator_namespace = all_sub_resp_obj.items[target_item]['metadata']['namespace']
+                assert 'channel' in all_sub_resp_obj.items[target_item]['spec'].keys()
+                assert 'sourceNamespace' in all_sub_resp_obj.items[target_item]['spec'].keys()
+                assert 'startingCSV' in all_sub_resp_obj.items[target_item]['spec'].keys()
+                assert 'name' in all_sub_resp_obj.items[target_item]['spec'].keys()
+                assert all_sub_resp_obj.items[target_item]['spec']['name'] == operator_name
+                assert all_sub_resp_obj.items[target_item]['status']['state'] == 'AtLatestKnown'
+                assert self.csv.is_cluster_service_version_present(csv_name, operator_namespace)
+                return all_sub_resp_obj.items[target_item]['spec']    
 
     def get_version_of_operator(self, operator_name: str) -> Optional[str]:
         """
